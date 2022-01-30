af012320509e1a67af769485179d877243cb7096
OmarZouari6 <zouari6omar@gmail.com>
2022-01-30 23:05:10 +0100

    omaroo

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React  from 'react';
+import face from './OMAR.jpg'
+function Home() {
+    return(
+<div className="home">
+<h1 className='title'>HELLO WORLD</h1>
+<img className='face' src={face} />
+
+</div>
+)
+}
+
+export default Home;
